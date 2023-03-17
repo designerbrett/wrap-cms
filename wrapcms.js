172ad63bb70947f2cd1e7427bdf3af39b7ef5570
designerbrett <brettdesigner@gmail.com>
2023-03-17 15:51:05 -0700

    Update wrapcms.js

--- wrapcms.js
+++ wrapcms.js
@@ -1,4 +1,3 @@
-// Add event listener to edit button
 var editButton = document.getElementById('edit-button');
 editButton.addEventListener('click', function(event) {
   event.preventDefault();
@@ -15,7 +14,6 @@ wrapcmsElements.forEach(function(element) {
   var wrapcmsType = element.getAttribute('wrapcms');
   var fieldName = element.getAttribute('field-name');
   var inputFields = getEditFormFields(wrapcmsType, fieldName, element);
-
 
   // Add the input fields to the edit form
   var editFormFieldsContainer = document.getElementById('edit-form-fields');
@@ -50,70 +48,49 @@ var editModal = document.getElementById('edit-modal');
 editModal.style.display = 'none';
 
 // Function to get the edit form fields based on the wrapcms element
-function getEditFormFields(wrapcmsType, element) {
-    var editFormFields = document.createElement('div');
-    var fieldLabel = document.createElement('label');
+function getEditFormFields(wrapcmsType, fieldName, element) {
+  var editFormFields = document.createElement('div');
+  var fieldLabel = document.createElement('label');
   fieldLabel.innerHTML = fieldName + ':';
   editFormFields.appendChild(fieldLabel);
 
-    var editIcon = document.createElement('i');
-    editIcon.classList.add('fas', 'fa-pencil-alt'); // Add your desired edit icon class here
-    editIcon.addEventListener('click', function(event) {
-      event.preventDefault();
-  
-      // Create the edit form dynamically based on the wrapcms element
-      var wrapcmsType = element.getAttribute('wrapcms');
-      var fieldName = element.getAttribute('field-name');
-      var editFormFields = getEditFormFields(wrapcmsType, fieldName, element);
-  
-      // Add the form fields to the edit form
-      var editFormFieldsContainer = document.getElementById('edit-form-fields');
-      editFormFieldsContainer.innerHTML = '';
-      editFormFieldsContainer.appendChild(editFormFields);
-  
-      // Show the edit modal
-      var editModal = document.getElementById('edit-modal');
-      editModal.style.display = 'block';
-    });
-   
-    switch (wrapcmsType) {
-        case 'title':
-          var titleInput = document.createElement('input');
-          titleInput.type = 'text';
-          titleInput.name = 'title';
-          titleInput.value = element.querySelector('h1').innerHTML;
-          titleInput.addEventListener('input', function(event) {
-            element.querySelector('h1').innerHTML = event.target.value;
-          });
-          editFormFields.appendChild(titleInput);
-          break;
-        case 'heading-2':
-            var titleInput = document.createElement('input');
-            titleInput.type = 'text';
-            titleInput.name = 'heading 2';
-            titleInput.value = element.querySelector('h2').innerHTML;
-            titleInput.addEventListener('input', function(event) {
-              element.querySelector('h2').innerHTML = event.target.value;
-            });
-            editFormFields.appendChild(titleInput);
-          break;
-        case 'content':
-          var contentTextarea = document.createElement('textarea');
-          contentTextarea.name = 'content';
-          contentTextarea.innerHTML = element.querySelector('p').innerHTML;
-          contentTextarea.addEventListener('input', function(event) {
-            element.querySelector('p').innerHTML = event.target.value;
-          });
-          editFormFields.appendChild(contentTextarea);
-          break;
-        // Add more cases for other wrapcms types
-    }
-    return editFormFields;
+  switch (wrapcmsType) {
+    case 'title':
+      var titleInput = document.createElement('input');
+      titleInput.type = 'text';
+      titleInput.name = 'title';
+      titleInput.value = element.querySelector('h1').innerHTML;
+      titleInput.addEventListener('input', function(event) {
+        element.querySelector('h1').innerHTML = event.target.value;
+      });
+      editFormFields.appendChild(titleInput);
+      break;
+    case 'heading-2':
+      var headingInput = document.createElement('input');
+      headingInput.type = 'text';
+      headingInput.name = 'heading 2';
+      headingInput.value = element.querySelector('h2').innerHTML;
+      headingInput.addEventListener('input', function(event) {
+        element.querySelector('h2').innerHTML = event.target.value;
+      });
+      editFormFields.appendChild(headingInput);
+      break;
+    case 'content':
+      var contentTextarea = document.createElement('textarea');
+      contentTextarea.name = 'content';
+      contentTextarea.innerHTML = element.querySelector('p').innerHTML;
+      contentTextarea.addEventListener('input', function(event) {
+        element.querySelector('p').innerHTML = event.target.value;
+      });
+      editFormFields.appendChild(contentTextarea);
+      break;
+    // Add more cases for other wrapcms types
   }
+  return editFormFields;
+}
 
 // Function to cancel editing and hide the modal
 function cancelEdit() {
   var editModal = document.getElementById('edit-modal');
   editModal.style.display = 'none';
 }
-
