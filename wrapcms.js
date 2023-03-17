57d286a34031144808f1bcb2657a088b24ba1f37
designerbrett <brettdesigner@gmail.com>
2023-03-17 14:04:05 -0700

    Update wrapcms.js

--- wrapcms.js
+++ wrapcms.js
@@ -47,35 +47,57 @@ editForm.addEventListener('submit', function(event) {
 
 // Function to get the edit form fields based on the wrapcms element
 function getEditFormFields(wrapcmsType, element) {
-  var editFormFields = document.createElement('span');
-  switch (wrapcmsType) {
-    case 'title':
-      var titleInput = document.createElement('input');
-      titleInput.type = 'text';
-      titleInput.name = 'title';
-      titleInput.value = element.querySelector('h1').innerHTML;
-      editFormFields.appendChild(titleInput);
-      break;
-    case 'heading-2':
-      var titleInput = document.createElement('input');
-      titleInput.type = 'text';
-      titleInput.name = 'heading 2';
-      titleInput.value = element.querySelector('h2').innerHTML;
-      editFormFields.appendChild(titleInput);
-      break;
-    case 'content':
-      var contentTextarea = document.createElement('textarea');
-      contentTextarea.name = 'content';
-      contentTextarea.innerHTML = element.querySelector('p').innerHTML;
-      editFormFields.appendChild(contentTextarea);
-      break;
-    // Add more cases for other wrapcms types
+    var editFormFields = document.createElement('span');
+    var editIcon = document.createElement('i');
+    editIcon.classList.add('fas', 'fa-pencil-alt'); // Add your desired edit icon class here
+    editIcon.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      // Create the edit form dynamically based on the wrapcms element
+      var wrapcmsType = element.getAttribute('wrapcms');
+      var editFormFields = getEditFormFields(wrapcmsType, element);
+  
+      // Add the form fields to the edit form
+      var editFormFieldsContainer = document.getElementById('edit-form-fields');
+      editFormFieldsContainer.innerHTML = '';
+      editFormFieldsContainer.appendChild(editFormFields);
+  
+      // Show the edit modal
+      var editModal = document.getElementById('edit-modal');
+      editModal.style.display = 'block';
+    });
+    editFormFields.appendChild(editIcon);
+    switch (wrapcmsType) {
+      case 'title':
+        var titleInput = document.createElement('input');
+        titleInput.type = 'text';
+        titleInput.name = 'title';
+        titleInput.value = element.querySelector('h1').innerHTML;
+        editFormFields.appendChild(titleInput);
+        break;
+      case 'heading-2':
+        var titleInput = document.createElement('input');
+        titleInput.type = 'text';
+        titleInput.name = 'heading 2';
+        titleInput.value = element.querySelector('h2').innerHTML;
+        editFormFields.appendChild(titleInput);
+        break;
+      case 'content':
+        var contentTextarea = document.createElement('textarea');
+        contentTextarea.name = 'content';
+        contentTextarea.innerHTML = element.querySelector('p').innerHTML;
+        editFormFields.appendChild(contentTextarea);
+        break;
+      // Add more cases for other wrapcms types
+    }
+    return editFormFields;
   }
-  return editFormFields;
-}
 
 // Function to cancel editing and hide the modal
 function cancelEdit() {
   var editModal = document.getElementById('edit-modal');
   editModal.style.display = 'none';
 }
+
+
+  
