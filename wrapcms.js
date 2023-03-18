ebe082a134fafc65f9bac044a5f065229675bb46
designerbrett <brettdesigner@gmail.com>
2023-03-17 20:41:32 -0700

    Update wrapcms.js

--- wrapcms.js
+++ wrapcms.js
@@ -94,3 +94,16 @@ function cancelEdit() {
   var editModal = document.getElementById('edit-modal');
   editModal.style.display = 'none';
 }
+
+// tinyimce
+tinymce.init({
+ selector: '#textarea',
+  plugins: [
+   'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+   'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+    'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+   ],
+    toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+    'alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+   });
