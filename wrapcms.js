b8c0242b0141ccc311a057c245f33d3de2faa133
designerbrett <brettdesigner@gmail.com>
2023-03-17 21:05:16 -0700

    Update wrapcms.js

--- wrapcms.js
+++ wrapcms.js
@@ -94,27 +94,3 @@ function cancelEdit() {
   var editModal = document.getElementById('edit-modal');
   editModal.style.display = 'none';
 }
-
-// tinyimce
-tinymce.init({
- selector: '#textarea',
-  plugins: [
-   'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-   'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-    'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-   ],
-    toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-    'alignleft aligncenter alignright alignjustify | ' +
-      'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
-   });
-
-
-import { generateHeader } from './header.js';
-import { generateFooter } from './footer.js';
-
-const header = generateHeader();
-const footer = generateFooter();
-
-document.body.prepend(header);
-document.body.append(footer);
-
