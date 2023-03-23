325e3b9738d1138d5cd60d04e5a373a3c78d28bd
designerbrett <brettdesigner@gmail.com>
2023-03-22 21:26:03 -0700

    removing files

--- archive-react/README.md
+++
@@ -1,2 +0,0 @@
-# wrap-cms
-A flat file cms that allows you to wrap HTML elements with a specific tag that enables you to edit that element in the admin panel.
--- archive-react/src/components/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Define a component for the editable content
-function Editable({ children }) {
-  return <span contentEditable>{children}</span>;
-}
